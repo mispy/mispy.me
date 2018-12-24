6e6fedf121d2399e37d2b29707cfdcf7917caeba
Jaiden Mispy <jaiden@mispy.me>
2018-12-24 17:56:27 +0800

    Add taxicab spheres link

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -39,7 +39,7 @@ export default class Homepage extends React.Component<{ assets: {[key: string]: 
                     <li>I made <a href="https://kanajolt.mispy.me/">Kanajolt</a>, a thing that helps you learn Japanese syllabaries by reading Pokémon names.</li>
                     <li>You can play <a href="https://pathspire.mispy.me/">Pathspire</a>, my little gamejam puzzle roguelike.</li>
                     <li>I'm trying to <a href="https://github.com/mispy/kiwisbybeat-archive">archive the works</a> of the mysterious webcomic artist Ryan Armand. If you happen to have pages from the Socks comics, let me know!</li>
-                    <li>Tiny web experiments: <a href="https://mispy-spectral-pulse.netlify.com">spectral pulse</a>, <a href="https://mispy-hexgrid.netlify.com/">hex gradients</a>, <a href="https://plusle.mispy.me/">positivity Plusle</a>, <a href="https://spdgrid.mispy.me/">spatial prisoner's dilemma</a></li>
+                    <li>Tiny web experiments: <a href="https://mispy.me/taxicab">taxicab spheres</a>, <a href="https://mispy-spectral-pulse.netlify.com">spectral pulse</a>, <a href="https://mispy-hexgrid.netlify.com/">hex gradients</a>, <a href="https://plusle.mispy.me/">positivity Plusle</a>, <a href="https://spdgrid.mispy.me/">spatial prisoner's dilemma</a></li>
                     <li>My <a href="https://github.com/mispy/adventofcode">solutions</a> to the Advent of Code competition puzzles</li>
                 </ul>
                 <h3>Writing</h3>
